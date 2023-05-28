9b17cb97ad68ed5f3cb73d7101f72ee9307d39ee
Vlad-Makarenko <Makrenkovladok@gmail.com>
2023-05-29 00:02:43 +0300

    project validation added

--- server/src/api/projects/validator.ts
+++ server/src/api/projects/validator.ts
@@ -3,11 +3,16 @@ import Joi from 'joi';
 export default class ProjectValidator {
 	create(body: { project: string; title: string; thumbnail?: string }) {
 		const schema = Joi.object({
-			title: Joi.string().alphanum().min(3).max(30).required().messages({
-				'*': 'Incorrect project title. Must consist of only letters and numbers. Length should be from 3 to 30 characters',
+			title: Joi.string()
+			.min(3)
+			.max(30)
+			.pattern(/^[0-9a-zA-Z\s]*$/)
+			.trim()
+			.messages({
+				"*": "Incorrect project title. Must consist of only letters and numbers. Length should be from 3 to 30 characters",
 			}),
-			project: Joi.string().min(2).max(30).required().messages({
-				'*': 'Incorrect project title. Must consist of only letters and numbers. Length should be from 3 to 30 characters',
+			project: Joi.string().min(2).required().messages({
+				'*': 'Incorrect project data.',
 			}),
 			thumbnail: Joi.any(),
 		});
