6762c2f3c0a957de32645f996952e2e51054303f
Vlad-Makarenko <Makrenkovladok@gmail.com>
2023-05-28 18:35:30 +0300

    Layes done

--- imagitor/src/settingBar/widgetList/LayerWidget.tsx
+++ imagitor/src/settingBar/widgetList/LayerWidget.tsx
@@ -1,28 +1,13 @@
-import React, { useEffect, useMemo, useState, useRef } from "react";
-import { Col, Figure, Form, Row, Container, ListGroup } from "react-bootstrap";
-import iconList from "../../config/icon.json";
-import alignStyles from "../../style/align.module.css";
-import sizeStyles from "../../style/size.module.css";
-import Drag from "../../util/Drag";
-import TRIGGER from "../../config/trigger";
-import { IconItemKind } from "../../view/object/icon";
-import useI18n from "../../hook/usei18n";
+import React, { useEffect, useState, useRef } from "react";
 import useItem from "../../hook/useItem";
-import { StageData, stageDataAction } from "../../redux/currentStageData";
-import { useDispatch } from "react-redux";
 import useSelection from "../../hook/useSelection";
-import { ITEMS_CONTEXT } from "../../hook/useItem";
-import useTransformer from "../../hook/useTransformer";
-import Konva from "konva";
 import { Node, NodeConfig } from "konva/lib/Node";
-import { Container as temp } from "konva/lib/Container";
-import useStage from "../../hook/useStage";
 import { WidgetKind } from "../Widget";
 import { SettingBarProps } from "..";
-import { Group } from "react-konva";
 import DraggableList from "react-draggable-list";
 import useHotkeyFunc from "../../hook/useHotkeyFunc";
 import { nanoid } from "nanoid";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 type LayerWidgetProps = {
   data: WidgetKind & SettingBarProps;
@@ -32,8 +17,8 @@ const LayerWidget: React.FC<LayerWidgetProps> = ({ data }) => {
   const [selectedItems, setSelectedItems] = useState<Node<NodeConfig>[]>([]);
   const [layersItems, setLayersItems] = useState<Node<NodeConfig>[]>([]);
   const [editingItem, setEditingItem] = useState<Node<NodeConfig> | null | undefined>(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [displayedName, setDisplayedName] = useState("");
+  const { deleteItems } = useHotkeyFunc();
+  const { setSelectedItems: setGlobalSelectedItems } = useSelection(data.transformer!);
 
   const { stageData, updateItem } = useItem();
   const { layerDown, layerUp } = useHotkeyFunc();
@@ -55,7 +40,9 @@ const LayerWidget: React.FC<LayerWidgetProps> = ({ data }) => {
   }, [data.selectedItems]);
 
   const handleClick = (item: Node<NodeConfig>, isCtrlPressed: boolean) => {
-    // handleInputBlur();
+    if (item.attrs.id === selectedItems[0]?.attrs.id) {
+      return;
+    }
     let updatedSelectedItems: Node<NodeConfig>[];
     if (isCtrlPressed) {
       if (selectedItems.includes(item)) {
@@ -70,28 +57,20 @@ const LayerWidget: React.FC<LayerWidgetProps> = ({ data }) => {
     data.onSelectItem!(undefined, updatedSelectedItems);
   };
 
-  const handleDoubleClick = (item: Node<NodeConfig>) => {
-    console.log("IN DOUBLE CLICK!", item);
-    setIsEditing(true);
-    setEditingItem(item);
-    setDisplayedName(item.attrs.displayedName);
+  const handleEditClick = (item: Node<NodeConfig>) => {
+    if (item) {
+      setEditingItem(item);
+    }
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDisplayedName(e.target.value);
-  };
-
-  const handleInputBlur = () => {
-    console.log("IN BLUR!");
-    if (editingItem?.attrs.name !== displayedName) {
-      updateItem(editingItem?.attrs.id, (attrs) => ({
+  const handleAcceptClick = (displayedName: string) => {
+    if (editingItem && displayedName) {
+      updateItem(editingItem.attrs.id, (attrs) => ({
         ...attrs,
         displayedName: displayedName,
       }));
     }
-    setIsEditing(false);
     setEditingItem(null);
-    setDisplayedName("");
   };
 
   const onMoveEnd = (newList: any, movedItem: any, oldIdx: number, newIdx: number) => {
@@ -110,6 +89,22 @@ const LayerWidget: React.FC<LayerWidgetProps> = ({ data }) => {
 
   return (
     <div className="tw-flex tw-h-[20vh] tw-w-full tw-flex-col tw-gap-4">
+      <div
+        className="w-full tw-flex tw-justify-end tw-gap-5 tw-pb-2 tw-pr-2 tw-shadow-sm"
+        style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.1)" }}
+      >
+        <OverlayTrigger
+          placement="bottom"
+          overlay={<Tooltip id={`tooltip_navbar-id_${data.id}`}>Delete layer</Tooltip>}
+        >
+          <div
+            className="tw-cursor-pointer"
+            onClick={() => deleteItems(data.selectedItems, setGlobalSelectedItems, data.transformer!.transformerRef!)}
+          >
+            <img alt={"pencil.svg"} src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/trash.svg`} />
+          </div>
+        </OverlayTrigger>
+      </div>
       <DraggableList
         itemKey="_id"
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -127,13 +122,10 @@ const LayerWidget: React.FC<LayerWidgetProps> = ({ data }) => {
               ref={ref as React.Ref<HTMLDivElement>}
               parentData={{
                 selectedItems,
-                isEditing,
                 editingItem,
                 handleClick,
-                handleDoubleClick,
-                handleInputChange,
-                handleInputBlur,
-                displayedName,
+                handleEditClick,
+                handleAcceptClick,
               }}
             />
           );
@@ -155,31 +147,24 @@ type ItemProps = {
   };
   parentData: {
     selectedItems: Node<NodeConfig>[];
-    isEditing: boolean;
     editingItem: Node<NodeConfig> | null | undefined;
-    displayedName: string;
+    handleAcceptClick: (ard: string) => void;
     handleClick: (item: Node<NodeConfig>, isCtrlPressed: boolean) => void;
-    handleDoubleClick: (item: Node<NodeConfig>) => void;
-    handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    handleInputBlur: () => void;
+    handleEditClick: (item: Node<NodeConfig>) => void;
   };
 };
 
 const Item = React.forwardRef<HTMLDivElement, ItemProps>(function ItemComponent(
-  { item, itemSelected, dragHandleProps, parentData },
+  { item, dragHandleProps, parentData },
   ref
 ) {
   const { onMouseDown, onTouchStart } = dragHandleProps;
-  const {
-    editingItem,
-    handleClick,
-    handleDoubleClick,
-    handleInputBlur,
-    handleInputChange,
-    isEditing,
-    selectedItems,
-    displayedName,
-  } = parentData;
+  const { editingItem, handleClick, handleAcceptClick, handleEditClick, selectedItems } = parentData;
+  const [displayedName, setDisplayedName] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDisplayedName(e.target.value);
+  };
 
   useEffect(() => {
     console.log(!editingItem?.attrs.id === item.attrs.id);
@@ -191,51 +176,39 @@ const Item = React.forwardRef<HTMLDivElement, ItemProps>(function ItemComponent(
       ref={ref}
       className={`${
         selectedItems.find((itemS) => itemS.attrs.id === item.attrs.id) && "tw-bg-blue-300"
-      } tw-flex tw-w-full tw-flex-row tw-items-center tw-gap-2 tw-rounded-md tw-p-2`}
+      } tw-flex tw-w-full tw-flex-row tw-items-center tw-gap-1 tw-rounded-md tw-p-2 tw-shadow-sm hover:tw-shadow-lg`}
+      style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.1)" }}
       onClick={(e) => handleClick(item, e.ctrlKey)}
-      onDoubleClick={() => handleDoubleClick(item)}
+      onDoubleClick={() => {
+        setDisplayedName(item.attrs.disabledName);
+        handleEditClick(item);
+      }}
     >
       <div className="tw-flex tw-w-full tw-flex-row tw-gap-2 tw-rounded-md tw-p-2">
         <img src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/Layers.svg`} alt={item.className} />
-        {/* {editingItem && isEditing ? ( */}
         <input
           type="text"
-          className="tw-w-2/3 tw-border-none tw-bg-transparent tw-outline-none tw-cursor-default tw-caret-transparent"
+          className={`${
+            editingItem?.attrs.id === item.attrs.id ? "" : "tw-caret-transparent"
+          } tw-w-2/3 tw-cursor-default tw-border-none tw-bg-transparent tw-outline-none`} //tw-caret-transparent
           disabled={!editingItem?.attrs.id === item.attrs.id}
           value={editingItem?.attrs.id === item.attrs.id ? displayedName : item.attrs.displayedName}
           onChange={handleInputChange}
-          // onBlur={handleInputBlur}
-          // autoFocus
-          // onFocus={(e) => e.target.select()}
+          onBlur={() => {
+            handleAcceptClick(displayedName);
+          }}
+          autoFocus={editingItem?.attrs.id === item.attrs.id}
         />
-        {/* ) : (
-          <span>{item.attrs.displayedName}</span>
-        )} */}
       </div>
-      {/* {editingItem?.attrs.id === item.attrs.id ? (
-        <div
-          className=""
-          onClick={(e) => {
-            handleInputBlur();
-          }}
-        >
-          <Figure.Image alt={"pencil.svg"} src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/check2.svg`} />
-        </div>
-      ) : (
-        <div
-          className=""
-          onClick={(e) => {
-            handleDoubleClick(item);
-          }}
-        >
-          <Figure.Image alt={"pencil.svg"} src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/pencil.svg`} />
-        </div>
-      )} */}
-      <div className="disable-select dragHandle" onTouchStart={onTouchStart} onMouseDown={onMouseDown}>
-        <Figure.Image
-          alt={"grip-vertical.svg"}
-          src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/grip-vertical.svg`}
-        />
+      <div
+        className="disable-select dragHandle tw-flex tw-items-center"
+        onTouchStart={onTouchStart}
+        onMouseDown={(e) => {
+          handleClick(item, false);
+          onMouseDown(e);
+        }}
+      >
+        <img alt={"grip-vertical.svg"} src={`${process.env.PUBLIC_URL}/assets/icon/bootstrap/grip-vertical.svg`} />
       </div>
     </div>
   );
