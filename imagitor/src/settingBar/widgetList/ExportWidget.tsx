65235a3d34486beb5637f0dc69f997f029d337c1
Vlad-Makarenko <Makrenkovladok@gmail.com>
2023-05-29 15:01:45 +0300

    export fixed

--- imagitor/src/settingBar/widgetList/ExportWidget.tsx
+++ imagitor/src/settingBar/widgetList/ExportWidget.tsx
@@ -1,16 +1,16 @@
+import { Group } from "konva/lib/Group";
+import { Node, NodeConfig } from "konva/lib/Node";
 import React from "react";
 import { Col, Figure, Row } from "react-bootstrap";
-import { Node, NodeConfig } from "konva/lib/Node";
-import { Group } from "konva/lib/Group";
+import { SettingBarProps } from "..";
 import exportList from "../../config/export.json";
+import useSelection from "../../hook/useSelection";
+import useStage from "../../hook/useStage";
+import useI18n from "../../hook/usei18n";
 import alignStyles from "../../style/align.module.css";
 import fontStyles from "../../style/font.module.css";
 import sizeStyles from "../../style/size.module.css";
 import { WidgetKind } from "../Widget";
-import { SettingBarProps } from "..";
-import useSelection from "../../hook/useSelection";
-import useStage from "../../hook/useStage";
-import useI18n from "../../hook/usei18n";
 
 export type ExportKind = {
   "data-item-type": string;
@@ -53,10 +53,11 @@ const ExportThumbnail: React.FC<{
   data: ExportKind;
 }> = ({ data }) => {
   const { getTranslation } = useI18n();
+
   const downloadSelected = (targetFrame?: Node<NodeConfig> | Group) => {
     const link = document.createElement("a");
-    const frame
-      = targetFrame ?? data.selectedItems.find((item) => item.attrs["data-item-type"] === "frame");
+    const frame = data.stageRef.current.getLayers()[0];
+
     if (frame) {
       const stage = frame.getStage()!;
       console.log(stage);
@@ -79,9 +80,7 @@ const ExportThumbnail: React.FC<{
   };
 
   const downloadAll = () => {
-    const frames = data.stageRef.current
-      .getChildren()[0]
-      .getChildren((item) => item.attrs.name === "label-group");
+    const frames = data.stageRef.current.getChildren()[0].getChildren((item) => item.attrs.name === "label-group");
     frames
       .map((frame) => (frame as Group).getChildren((item) => item.attrs.name === "label-target")[0])
       .forEach((frame) => {
@@ -101,10 +100,10 @@ const ExportThumbnail: React.FC<{
     <Figure
       as={Col}
       onClick={onClickDownload(data.id)}
-      className={[alignStyles.absoluteCenter, alignStyles.wrapTrue].join(" ")}>
+      className={[alignStyles.absoluteCenter, alignStyles.wrapTrue].join(" ")}
+    >
       <i className={`bi-${data.icon}`} style={{ fontSize: 20, width: 25 }} />
-      <Figure.Caption
-        className={[fontStyles.font075em, sizeStyles.width100, "text-center"].join(" ")}>
+      <Figure.Caption className={[fontStyles.font075em, sizeStyles.width100, "text-center"].join(" ")}>
         {`${getTranslation("widget", "export", data.id, "name")}`}
       </Figure.Caption>
     </Figure>
